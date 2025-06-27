34f63e6b107ddf32aab6742d15c819d7541f8005
Daniel Ephrat <dephrat@uwaterloo.ca>
2025-06-27 09:12:10 -0400

    input validation

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 //structure: main writes a shell to gather info and validate, then pass it to the calendar function.
 
+#include <ctime>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <vector>
 
@@ -14,7 +16,7 @@ bool is_leap_year(const unsigned int);
 unsigned int calendar(const unsigned int, const unsigned int, const unsigned int);
 unsigned int num_leap_years(const unsigned int end, const unsigned int start = 0);
 
-const unsigned int jan_1_0000 = 6; //idk, i'm putting something random here for now
+const unsigned int jan_1_0000 = 6;
 
 const vector<string> day_map = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 const vector<string> month_map = {"January", "February", "March", "April", "May", "June", "July", "August", 
@@ -26,23 +28,27 @@ const unordered_map<string, int> month_day_map = {{"January", 0}, {"February", 1
 const vector<int> month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
-    unsigned int month = 6;
-    unsigned int date = 1;
-    unsigned int year = 2025;
+    unsigned int year, month, date;
+    string year_str, month_str, date_str;
 
-
-    string month_str;
     while (true) {
         //get year
         cout << "Year: ";
-        cin >> year;
+        cin >> year_str;
+        try {
+            year = stoi(year_str);
+        } catch (...) {
+            cout << "Invalid year. Starting over." << endl;
+            continue;
+        }
+
 
         //get month
         cout << "Month: ";
         cin >> month_str;
         //validate month
         if (month_day_map.find(month_str) == month_day_map.end()) {
-            cout << "Unrecognized month. Start over." << endl;
+            cout << "Unrecognized month. Starting over." << endl;
             continue;
         }
         //convert month
@@ -50,10 +56,16 @@ int main() {
 
         //get date
         cout << "Date: ";
-        cin >> date;
+        cin >> date_str;
+        try {
+            date = stoi(date_str);
+        } catch (...) {
+            cout << "Invalid date. Starting over." << endl;
+            continue;
+        }
         //How do we check if the reading is a valid unsigned int?
         //validate date
-        if (date > month_days[month]) {
+        if (date > month_days[month] || date < 1) {
             if (month != 1) {
                 cout << "Date too high. Starting over." << endl;
                 continue;
@@ -68,14 +80,27 @@ int main() {
             }
         }
 
-        //calculate day of week
-        string day_of_week = day_map[calendar(month, date, year)];
-        cout << "Day of week is: " << day_of_week << endl << endl;
+        //output their date for their validation
+        cout << "Day of the week on " << month_map[month] << " " << date << ", " << year;
+
+        time_t timestamp;
+        time(&timestamp);
+        tm cd = *localtime(&timestamp);
+
+        tuple<int,int,int> current = make_tuple(cd.tm_year + 1900, cd.tm_mon, cd.tm_mday);
+        tuple<int,int,int> user_date = make_tuple(year, month, date);
+        if (user_date < current) cout << " was ";
+        else if (user_date == current) cout << " is ";
+        else cout << " will be ";
+        cout << day_map[calendar(year, month, date)] << "." << endl << endl;
+        for (int i = 0; i < 1000000; ++i) {
+            calendar(year, month, date);
+        }
     }
 
 }
 
-unsigned int calendar(const unsigned int month, const unsigned int date, const unsigned int year) {
+unsigned int calendar(const unsigned int year, const unsigned int month, const unsigned int date) {
 
 //get jan 1 in the desired year
 int num_days_added = 365 * year; //does this fit inside int?
@@ -89,7 +114,6 @@ int num_leap_year_days = num_leap_years(year); //num_leap_years goes from 0 incl
 // you forget what I mean. if you include start, the function will calculate including start and excluding end.
 // num_leap_years counts number of leap days from jan1 of start to jan1 of end. if start == end, then
 // num leap days is 0.
-cout << "leap year days " << num_leap_year_days << endl; 
 
 num_days_added += num_leap_year_days;
 
@@ -99,12 +123,9 @@ int jan_1_year = (jan_1_0000 + num_days_added) % 7;
 num_days_added = 0;
 for (int i = 0; i < month; i++) {
     num_days_added += month_days[i];
-    //cout << "adding stuff: " << month_days[i] << endl;
 }
 
 num_days_added += (month > 1 && is_leap_year(year)); // if it's past february and we're in a leap year, add a day.
-
-//cout << "month num days added: " << num_days_added << endl;
 
 int month_1_year = (jan_1_year + num_days_added) % 7;
 
